d3a9702ce0ec39ee8af6a2a8dd5f3c28b9aad931
Karina Hristova <52160729+karinaghristova@users.noreply.github.com>
2023-01-08 20:36:17 +0200

    First task now works with input from the console

--- 82133_Project/FirstTask.cpp
+++ 82133_Project/FirstTask.cpp
@@ -1,89 +1,96 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include <algorithm>
 #include "skip_list.h"
 
-
-//First task functions
-void initializeCitiesInSkipList(SkipList<std::string>& sl) {
-	sl.insertEnd("Sofia");
-	sl.insertEnd("Pazardzhik");
-	sl.insertEnd("Plovdiv");
-	sl.insertEnd("Dimitrovgrad");
-	sl.insertEnd("StaraZagora");
-	sl.insertEnd("NovaZagora");
-	sl.insertEnd("Yambol");
-	sl.insertEnd("Karnobat");
-	sl.insertEnd("Burgas");
+void setExpresses(SkipList<std::string>& sl, std::string first, std::string second) {
+	sl.setExpressToNode(sl.getNodeWithValue(first), sl.getNodeWithValue(second));
 }
 
-void setExpresses(SkipList<std::string>& sl) {
-	sl.setExpressToNode(sl.getNodeWithValue("Sofia"), sl.getNodeWithValue("Plovdiv"));
-	sl.setExpressToNode(sl.getNodeWithValue("Plovdiv"), sl.getNodeWithValue("NovaZagora"));
-	sl.setExpressToNode(sl.getNodeWithValue("Dimitrovgrad"), sl.getNodeWithValue("NovaZagora"));
-	sl.setExpressToNode(sl.getNodeWithValue("StaraZagora"), sl.getNodeWithValue("Yambol"));
-	sl.setExpressToNode(sl.getNodeWithValue("NovaZagora"), sl.getNodeWithValue("Burgas"));
+void initiateCitiesThatMustBeVisited(LinkedList<std::string>& wantToVisit, std::vector<std::string> cities) {
+	size_t numberOfCities = cities.size();
+	for (size_t i = 0; i < numberOfCities; i++)
+		wantToVisit.insertEnd(cities[i]);
 }
 
-void setExpressesSecondOption(SkipList<std::string>& sl) {
-	sl.setExpressToNode(sl.getNodeWithValue("Sofia"), sl.getNodeWithValue("Dimitrovgrad"));
-	sl.setExpressToNode(sl.getNodeWithValue("Pazardzhik"), sl.getNodeWithValue("Dimitrovgrad"));
-	sl.setExpressToNode(sl.getNodeWithValue("Dimitrovgrad"), sl.getNodeWithValue("NovaZagora"));
-	sl.setExpressToNode(sl.getNodeWithValue("StaraZagora"), sl.getNodeWithValue("Yambol"));
-	sl.setExpressToNode(sl.getNodeWithValue("NovaZagora"), sl.getNodeWithValue("Burgas"));
-}
+std::vector<std::string> splitLine(std::string line) {
+	std::vector<std::string> words;
+	std::string word;
 
-void initiateCitiesThatMustBeVisited(LinkedList<std::string>& wantToVisit) {
-	wantToVisit.insertEnd("Plovdiv");
-	wantToVisit.insertEnd("StaraZagora");
-	wantToVisit.insertEnd("Yambol");
-}
+	size_t pos = 0;
+	while ((pos = line.find(' ')) != std::string::npos) {
+		word = line.substr(0, pos);
+		line.erase(0, pos + 1);
+		words.push_back(word);
+	}
+	words.push_back(line);
 
-void initiateCitiesThatMustBeVisitedSecondOption(LinkedList<std::string>& wantToVisit) {
-	wantToVisit.insertEnd("Pazardzhik");
-	wantToVisit.insertEnd("Dimitrovgrad");
-	wantToVisit.insertEnd("Yambol");
-}
-
-void initiateCitiesThatMustBeVisitedThirdOption(LinkedList<std::string>& wantToVisit) {
-	wantToVisit.insertEnd("Plovdiv");
-	wantToVisit.insertEnd("StaraZagora");
-	wantToVisit.insertEnd("Yambol");
-}
-
-void initiateCitiesThatMustBeVisitedFourthOption(LinkedList<std::string>& wantToVisit) {
-	wantToVisit.insertEnd("Dimitrovgrad");
-	wantToVisit.insertEnd("NovaZagora");
-	wantToVisit.insertEnd("Karnobat");
+	return words;
 }
 
 void runFirstTask() {
 	//1. Initiating the cities
 	SkipList<std::string> sl = SkipList<std::string>();
-	initializeCitiesInSkipList(sl);
-	std::cout << "All train stations:\n";
-	sl.printList();
+	std::cout << "Enter number of cities: ";
+	int numberOfCities = 0;
+	std::cin >> numberOfCities;
+	std::cout << "Please enter the names of " << numberOfCities << " cities separated by whitespaces in one line.\n";
+	std::cout << "------------------------------------------------------\n";
+	std::string city;
+	for (size_t i = 0; i < numberOfCities; i++)	{
+		std::cin >> city;
+		sl.insertEnd(city);
+	}
 
-	std::cout << "\n------------------\n\n";
+	std::cout << "------------------------------------------------------\n";
+	//std::cout << "All train stations:\n";
+	//sl.printList();
+	//std::cout << "------------------------------------------------------\n";
 
 	//2. Setting expresses
-	setExpresses(sl);
-	std::cout << "Express train stations only:\n";
-	sl.printWithExpressOnly();
+	std::cout << "Please enter information about all express trains in the following format: {Start point city} {End point city}\n";
+	std::cout << "When you are done please write \'EXIT\'\n";
+	std::cout << "------------------------------------------------------\n";
 
-	std::cout << "\n------------------\n\n";
+	std::string firstCity, secondCity;
+	while (true) {
+		std::cin >> firstCity;
+		std::string firstCityLowercase = firstCity;
+		std::transform(firstCityLowercase.begin(), firstCityLowercase.end(),
+						firstCityLowercase.begin(), ::tolower);
+		if (firstCityLowercase == "exit")
+			break;
+		std::cin >> secondCity;
+		setExpresses(sl, firstCity, secondCity);
+	}
+	std::cout << "------------------------------------------------------\n";
+	//std::cout << "Express train stations only:\n";
+	//sl.printWithExpressOnly();
+	//std::cout << "------------------------------------------------------\n";
 
 	//3. Initiating a list of the cities we want to visit
 	LinkedList<std::string> wantToVisit = LinkedList<std::string>();
-	initiateCitiesThatMustBeVisited(wantToVisit);
+	std::cout << "Please enter the names of the cities you want to visit separated by whitespaces.\n";
+	std::cout << "------------------------------------------------------\n";
+	std::vector<std::string> citiesToBeVisited;
+	std::string line;
+	std::cin.ignore();
+	std::getline(std::cin, line);
+	citiesToBeVisited = splitLine(line);
+	initiateCitiesThatMustBeVisited(wantToVisit, citiesToBeVisited);
+	std::cout << "------------------------------------------------------\n";
+	//std::cout << "List of the cities you want to visit:\n";
+	//wantToVisit.printList();
+	//std::cout << "------------------------------------------------------\n";
 
 	//4. Creating the route
 	LinkedList<std::string> result = sl.createRoute(&wantToVisit);
 	std::cout << "Calculated route:\n";
 	result.printList();
+	std::cout << "------------------------------------------------------\n";
+
 }
-
-
 int main()
 {
 	//First task should read from the input
@@ -91,12 +98,3 @@ int main()
 	runFirstTask();
 	return 0;
 }
-
-//First express and first cities option expected output:
-//Sofia, Plovdiv, Dimitrovgrad, StaraZagora, Yambol, Karnobat, Burgas,
-//First express and second cities option expected output: 
-//Sofia, Pazardzhik, Plovdiv, Dimitrovgrad, NovaZagora, Yambol, Karnobat, Burgas
-//Second express option and third cities expected output: 
-//Sofia, Pazardzhik, Plovdiv, Dimitrovgrad, StaraZagora, Yambol, Karnobat, Burgas
-//Second express and fourth cities option expected output: 
-//Sofia, Dimitrovgrad, NovaZagora, Yambol, Karnobat, Burgas
